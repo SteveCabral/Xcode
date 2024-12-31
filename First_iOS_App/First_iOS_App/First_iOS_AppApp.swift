3d704a54897621b29ea53d78890eca05aef87527
Steve <SteveCabral@hotmail.com>
2024-12-30 20:01:35 -0500

    Xcode app on Mac Mini

---
+++ First_iOS_App/First_iOS_App/First_iOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_iOS_AppApp.swift
+//  First_iOS_App
+//
+//  Created by Steve Cabral on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct First_iOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ First_macOS_command_line_app_Swift/First_macOS_command_line_app_Swift/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  First_macOS_command_line_app_Swift
+//
+//  Created by Steve Cabral on 6/18/24.
+//
+
+import Foundation
+
+print("Hello, World!")
+
